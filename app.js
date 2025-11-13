408ca23b75bafc26686ed94f63bf68cfb68ad8c7
Timthom <monsson47thomas@gmail.com>
2025-11-13 16:31:34 +0100

    Second commit

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 console.log("funkar");
+
+
+/*forEach*/
+
+let numbers = [1, 2, 3];
+
+numbers.forEach(number => {
+    console.log(number);
+})
+
+/**map */
+
+let dollar = [1, 2, 3];
+
+let euros = dollar.map(value => value * 0.95);
+
+console.log(euros);
+
+/**reduce */
+
+let moreNumbers = [1, 2, 3];
+
+let sum = moreNumbers.reduce((total, current) => {
+    return total + current
+}, 0)
+
+console.log(sum);
+
+let concat = moreNumbers.reduce((total, current) => {
+    return total + current
+}, '')
+
+console.log(concat);
